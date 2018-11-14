2c7557c00df38de42ce78b88e06cae68a5b713c5
Ishu Khanchi <ishukhanchi@gmail.com>
2018-11-14 13:11:54 +0530

    authentication added

--- submit.js
+++ submit.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
+var querystring = require('querystring');
 
 var server = http.createServer(function (request, response) {
-    
+
     if (request.url="/submit") {
         fs.readFile("./index.html", function (error, pgResp) {
             if (error) {
@@ -11,16 +12,31 @@ var server = http.createServer(function (request, response) {
             } else {
                 response.writeHead(200, { 'Content-Type': 'text/html' });
                 response.write(pgResp);
-            } 
+            }
             response.end();
         });
-        if(request.username==="username"&&request.password==="password"){
-        	console.log('user is authenticated');
+        console.log()
+       if(request.method== "POST")
+        {
+          var a = "";
+          request.on("data", function(part)
+          {
+            a += part;
+          });
+          request.on("end", function(part)
+          {
+            var store = querystring.parse(a);
+        
+            if(store.username === "username" && store.password === "password")
+            {
+              console.log("User is authenticated");
+           }
+            else {
+               console.log("User is not authenticated.");
+            }
+          });
         }
-        else{
-        	console.log('user is not authenticated');
-        }
-    } 
+    }
 });
 server.listen(8080);
 console.log('Server listening on port 8080');
